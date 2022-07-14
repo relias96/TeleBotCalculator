1a84f872c05dbd033cb918c6484028c745844325
relias96 <102990749+relias96@users.noreply.github.com>
2022-07-14 11:44:06 +0200

    Update PyBot.py

--- PyBot.py
+++ PyBot.py
@@ -4,3 +4,21 @@ with open('API_token.txt') as f:
    API_TOKEN = f.readlines()
 
 bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
