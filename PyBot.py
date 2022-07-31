f865b31b011b92032857fdeb3def01fb16f169c6
relias96 <102990749+relias96@users.noreply.github.com>
2022-07-31 10:44:05 +0200

    Update PyBot.py

--- PyBot.py
+++ PyBot.py
@@ -10,8 +10,14 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Hi there, I am TeleBotCalculator.\
+I can reply to the following commands:\
+- simplify\
+- derivate\
+- integrate\
+- plot\
+- maximum\
+- minimum\ 
 """)
 
 
